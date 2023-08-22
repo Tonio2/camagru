2eee2c913080a82bcf85e0d82c653a1deb68ebd6
Antoine Labalette <labalette.antoine@gmail.com>
2023-08-22 13:30:55 +0200

    Set up a dockerized env for php and mysql

---
+++ www/index.php
@@ -0,0 +1,26 @@
+<?php
+$host = "db";
+$db = $_ENV["MYSQL_DATABASE"];
+$user = $_ENV["MYSQL_USER"];
+$password = $_ENV["MYSQL_PASSWORD"];
+
+$conn = mysqli_connect($host, $user, $password, $db);
+
+if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT 'Hello love!' as hello";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	while ($row = $result->fetch_assoc()) {
+		echo $row["hello"];
+	}
+} else {
+	echo 'No result';
+}
+
+$conn->close();
+?>
