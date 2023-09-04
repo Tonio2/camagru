e3d534454ec1c674a30e42686648e1c88676f518
Antoine Labalette <labalette.antoine@gmail.com>
2023-09-04 04:52:25 +0200

    edit image in backend with alpha image

--- www/html/upload_data.php
+++ www/html/upload_data.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["picture"])) {
 	$info = finfo_open(FILEINFO_MIME);
 	$mime = finfo_file($info, $filename);
 	finfo_close($info);
-	$îsValidMime = false;
+	$isValidMime = false;
 	foreach ($allowedMimes as $allowedMime) {
 		if (strpos($mime, $allowedMime) === 0) {
 			$isValidMime = true;
@@ -45,6 +45,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["picture"])) {
 		$response["msg"] = "File too large";
 	} elseif (getimagesize($filename)) {
 		$image = imagecreatefromstring(file_get_contents($filename));
+		$alpha_image = imagecreatefrompng("image1.png");
+		if ($image && $alpha_image) {
+			// Get dimensions of uploaded and alpha images
+			list($width, $height) = getimagesize($filename);
+			list($alpha_width, $alpha_height) = getimagesize("image1.png");
+
+			imagealphablending($image, true);
+        imagesavealpha($image, true);
+
+        // Resize alpha image to match uploaded image dimensions, if needed
+        if ($alpha_width != $width || $alpha_height != $height) {
+            $alpha_image_resized = imagecreatetruecolor($width, $height);
+            imagealphablending($alpha_image_resized, false);
+            imagesavealpha($alpha_image_resized, true);
+            imagecopyresampled($alpha_image_resized, $alpha_image, 0, 0, 0, 0, $width, $height, $alpha_width, $alpha_height);
+            $alpha_image = $alpha_image_resized;
+        }
+
+        // Merge the images pixel by pixel to respect alpha channel
+        for ($x = 0; $x < $width; $x++) {
+            for ($y = 0; $y < $height; $y++) {
+                $alpha_pixel = imagecolorat($alpha_image, $x, $y);
+                $alpha = ($alpha_pixel >> 24) & 0xFF;
+                if ($alpha < 127) { // check if the pixel is transparent
+                    imagesetpixel($image, $x, $y, $alpha_pixel);
+                }
+            }
+        }
+		}
 		if (imagejpeg($image, $targetPath, 85)) {
 			$sql = "INSERT INTO pictures(user_id, src) VALUES(?, ?)";
 			$stmt = $conn->prepare($sql);
@@ -69,3 +98,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["picture"])) {
 header("Content-Type: application/json");
 echo json_encode($response);
 exit();
+
+?>
