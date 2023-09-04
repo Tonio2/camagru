40ff062ff16022db1f509e22b1a34d664f0d78c0
Antoine Labalette <labalette.antoine@gmail.com>
2023-09-04 02:22:00 +0200

    update dockerization

--- www/entrypoint.sh
+++ www/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# Create uploads directory and allow apache to access it
 mkdir -p /var/www/uploads
-
 chown -R www-data:www-data /var/www/uploads
 chmod 755 /var/www/uploads
+
+# Same for log file
+[ ! -f "/var/www/file.log" ] && touch "/var/www/file.log"
+chown -R www-data:www-data /var/www/file.log
+chmod 755 /var/www/file.log
 
 apache2-foreground
