57e4453f38b02379efdea3f8b3546d31af0b8aad
Antoine Labalette <labalette.antoine@gmail.com>
2023-09-06 16:59:55 +0200

    add mail to notif

--- www/html/add_comment.php
+++ www/html/add_comment.php
@@ -3,6 +3,7 @@
 require_once "../config/config.php";
 require_once "../classes/session.php";
 require_once "../classes/database.php";
+require_once "../utils/mail.php";
 
 $session = new Session();
 $session->require_auth();
@@ -21,6 +22,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["pictureId"]) && isset
 	$stmt->bind_param("iis", $userId, $pictureId, $comment);
 	if ($stmt->execute()) {
 		$response["success"] = true;
+		$sql = "SELECT u.email AS mail, u.email_preference AS notificationPreference, p.id FROM users as u LEFT JOIN pictures AS p ON p.user_id = u.id WHERE p.id = ?";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("i", $pictureId);
+		$stmt->execute();
+		$res = $stmt->get_result();
+		$row = $res->fetch_assoc();
+		if ($row["notificationPreference"]) {
+			sendMail($row["mail"], "Notification", "Your picture has received a new comment");
+		}
 	}
 }
 header("Content-Type: application/json");
